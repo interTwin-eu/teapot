0422675c46071df644ceee5e06c916b74b5ade99
vrbanecd <dijana.vrbanec@desy.de>
2023-10-27 10:36:13 +0200

    Debug

--- robot/keycloak-config.py
+++ robot/keycloak-config.py
@@ -1,7 +1,7 @@
 from keycloak import KeycloakAdmin, KeycloakOpenID, KeycloakOpenIDConnection
 
 # Configure client
-keycloak_openid = KeycloakOpenID(server_url="http://keycloak/auth/",
+keycloak_openid = KeycloakOpenID(server_url="keycloak",
                                  client_id="test_client",
                                  realm_name="test_realm",
                                  client_secret_key="secret-key")
@@ -28,7 +28,7 @@ token = keycloak_openid.token("user", "password")
 userinfo = keycloak_openid.userinfo(token['access_token'])
 
 keycloak_connection = KeycloakOpenIDConnection(
-                        server_url="http://keycloak/",
+                        server_url="keycloak",
                         username='admin',
                         password='testing1',
                         realm_name="test_realm",
