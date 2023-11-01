b252938d6e8f892ed94fc0c9a7dc5089a5f2d270
vrbanecd <dijana.vrbanec@desy.de>
2023-11-01 13:43:06 +0100

    Test1.13

--- robot/keycloak-config.py
+++ robot/keycloak-config.py
@@ -1,35 +1,47 @@
 from keycloak import KeycloakAdmin, KeycloakOpenID, KeycloakOpenIDConnection
 
-#Keycloak Admin
+# Keycloak Admin
 keycloak_connection = KeycloakOpenIDConnection(
                         server_url="http://keycloak:8080/",
                         username='admin',
                         password='testing1',
                         realm_name="master",
-                        user_realm_name="test-realm",
                         verify=True)
 
 keycloak_admin = KeycloakAdmin(connection=keycloak_connection)
 
-##Changing Realm
-#keycloak_admin.create_realm(payload={"realm": "test-realm"}, skip_exists=False)
-#keycloak_connection.realm_name = "test-realm"
+# Changing Realm
+keycloak_admin.create_realm(payload={"realm": "test-realm"}, skip_exists=False)
+keycloak_connection.realm_name = "test-realm"
 
-#Configuring client in the new Realm
+# Configuring client in the new Realm
 keycloak_openid = KeycloakOpenID(server_url="http://keycloak:8080/",
                                  client_id="test-client",
                                  realm_name="test-realm",
                                  client_secret_key="test-secret")
 
-#Get WellKnown
+# Keycloak admin with new realm and a new client
+keycloak_connection1 = KeycloakOpenIDConnection(
+                        server_url="http://keycloak:8080/",
+                        username='admin',
+                        password='testing1',
+                        realm_name="master",
+                        user_realm_name="test-realm",
+                        client_id="test_client",
+                        client_secret_key="test-secret",
+                        verify=True)
+
+keycloak_admin1 = KeycloakAdmin(connection=keycloak_connection1)
+
+# Get WellKnown
 config_well_known = keycloak_openid.well_known()
 
-#Adding Users
-test_user1 = keycloak_admin.create_user({"username": "test-user1",
+# Adding Users
+test_user1 = keycloak_admin1.create_user({"username": "test-user1",
                                          "enabled": True,
                                          "credentials": [{"value": "secret1",
                                                           "type": "password"}]})
-test_user2 = keycloak_admin.create_user({"username": "test-user2",
+test_user2 = keycloak_admin1.create_user({"username": "test-user2",
                                          "enabled": True,
                                          "credentials": [{"value": "secret2",
                                                           "type": "password"}]})
@@ -51,4 +63,3 @@ token = keycloak_openid.token("user", "password")
 
 # Get Userinfo
 userinfo = keycloak_openid.userinfo(token['access_token'])
-
