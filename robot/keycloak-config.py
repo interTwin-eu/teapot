a3d467d3318e7149fba09814c4fd1ef9fbc0e0fb
vrbanecd <dijana.vrbanec@desy.de>
2023-11-01 10:34:49 +0100

    Test1.2

--- robot/keycloak-config.py
+++ robot/keycloak-config.py
@@ -2,12 +2,25 @@ from keycloak import KeycloakAdmin, KeycloakOpenID, KeycloakOpenIDConnection
 
 # Configure client
 keycloak_openid = KeycloakOpenID(server_url="http://keycloak:8080/",
-                                 client_id="example_client",
-                                 realm_name="example_realm",
-                                 client_secret_key="secret")
+                                 client_id="test-client",
+                                 realm_name="test-realm",
+                                 client_secret_key="test-secret")
+
+keycloak_connection = KeycloakOpenIDConnection(
+                        server_url="http://keycloak:8080/",
+                        username='admin',
+                        password='testing1',
+                        realm_name="test-realm",
+                        client_id="test-client",
+                        client_secret_key="test-secret",
+                        verify=True)
+
+keycloak_admin = KeycloakAdmin(connection=keycloak_connection)
+
+
 
 # Get WellKnown
-config_well_known = keycloak_openid.well_known()
+#config_well_known = keycloak_openid.well_known()
 
 ## Get Code With Oauth Authorization Request
 #auth_url = keycloak_openid.auth_url(
@@ -27,16 +40,6 @@ config_well_known = keycloak_openid.well_known()
 ## Get Userinfo
 #userinfo = keycloak_openid.userinfo(token['access_token'])
 #
-#keycloak_connection = KeycloakOpenIDConnection(
-#                        server_url="http://keycloak:8080/",
-#                        username='admin',
-#                        password='testing1',
-#                        realm_name="test_realm",
-#                        client_id="test_client",
-#                        client_secret_key="secret-key",
-#                        verify=True)
-#
-#keycloak_admin = KeycloakAdmin(connection=keycloak_connection)
 #
 #test_user1 = keycloak_admin.create_user({"username": "test_user1",
 #                                         "enabled": True,
