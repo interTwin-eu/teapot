94c5f7773411e36c46af79754e015ada08e1947b
vrbanecd <dijana.vrbanec@desy.de>
2023-11-01 14:12:08 +0100

    Test1.16

--- robot/keycloak-config.py
+++ robot/keycloak-config.py
@@ -36,6 +36,24 @@ keycloak_admin = KeycloakAdmin(connection=keycloak_connection)
 # Get WellKnown
 config_well_known = keycloak_openid.well_known()
 
+# Get Code With Oauth Authorization Request
+auth_url = keycloak_openid.auth_url(
+    redirect_uri="http://localhost:4242/",
+    scope="email",
+    state="your_state_info")
+
+# Get Access Token With Code
+access_token = keycloak_openid.token(
+    grant_type='authorization_code',
+    code='the_code_you_get_from_auth_url_callback',
+    redirect_uri="http://localhost:4242/")
+
+# Get Token
+token = keycloak_openid.token("user", "password")
+
+# Get Userinfo
+userinfo = keycloak_openid.userinfo(token['access_token'])
+
 # Adding Users
 test_user1 = keycloak_admin.create_user({"username": "test-user1",
                                           "enabled": True,
@@ -45,21 +63,3 @@ test_user2 = keycloak_admin.create_user({"username": "test-user2",
                                           "enabled": True,
                                           "credentials": [{"value": "secret2",
                                                            "type": "password"}]})
-
-# Get Code With Oauth Authorization Request
-auth_url = keycloak_openid.auth_url(
-    redirect_uri="http://keycloak:4242/",
-    scope="email",
-    state="your_state_info")
-
-# Get Access Token With Code
-access_token = keycloak_openid.token(
-    grant_type='authorization_code',
-    code='the_code_you_get_from_auth_url_callback',
-    redirect_uri="http://keycloak:4242/")
-
-# Get Token
-token = keycloak_openid.token("user", "password")
-
-# Get Userinfo
-userinfo = keycloak_openid.userinfo(token['access_token'])
