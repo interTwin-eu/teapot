0403446af02f4a76a94f7f32f5201c0ebcb5af75
vrbanecd <dijana.vrbanec@desy.de>
2023-11-01 10:20:29 +0100

    Test1.1

--- robot/keycloak-config.py
+++ robot/keycloak-config.py
@@ -2,9 +2,9 @@ from keycloak import KeycloakAdmin, KeycloakOpenID, KeycloakOpenIDConnection
 
 # Configure client
 keycloak_openid = KeycloakOpenID(server_url="http://keycloak:8080/",
-                                 client_id="TestClient",
-                                 realm_name="TestRealm",
-                                 client_secret_key="test-key")
+                                 client_id="example_client",
+                                 realm_name="example_realm",
+                                 client_secret_key="secret")
 
 # Get WellKnown
 config_well_known = keycloak_openid.well_known()
