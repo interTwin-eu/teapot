32b75ffba16debf14d6ef40e98bd52dba3149384
vrbanecd <dijana.vrbanec@desy.de>
2023-11-01 13:20:04 +0100

    Test1.11

--- robot/keycloak-config.py
+++ robot/keycloak-config.py
@@ -14,20 +14,22 @@ keycloak_admin = KeycloakAdmin(connection=keycloak_connection)
 keycloak_admin.create_realm(payload={"realm": "test-realm"}, skip_exists=False)
 keycloak_connection.realm_name = "test-realm"
 
-keycloak_connection = KeycloakOpenIDConnection(
-                        server_url="http://keycloak:8080/",
-                        username='admin',
-                        password='testing1',
-                        realm_name="test-realm",
-                        verify=True)
-
-keycloak_admin = KeycloakAdmin(connection=keycloak_connection)
-
 #Configuring client in the new Realm
 keycloak_openid = KeycloakOpenID(server_url="http://keycloak:8080/",
                                  client_id="test-client",
                                  realm_name="test-realm",
                                  client_secret_key="test-secret")
+
+keycloak_connection = KeycloakOpenIDConnection(
+                        server_url="http://keycloak:8080/",
+                        username='admin',
+                        password='testing1',
+                        realm_name="test-realm",
+                        client_id="test-client",
+                        client_secret_key="test-secret",
+                        verify=True)
+
+keycloak_admin = KeycloakAdmin(connection=keycloak_connection)
 
 #Get WellKnown
 config_well_known = keycloak_openid.well_known()
