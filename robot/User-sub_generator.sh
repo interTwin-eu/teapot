0528839d53d6bd8e57b47067120df043a58c28a3
vrbanecd <dijana.vrbanec@desy.de>
2023-11-06 16:08:25 +0100

    Add configurations for testing

---
+++ robot/User-sub_generator.sh
@@ -0,0 +1,11 @@
+user1=test-user1
+curl -s -H "Authorization: Bearer $(oidc-token test-user1)" http://keycloak:8080/realms/test-realm/protocol/openid-connect/userinfo | jq . | grep sub | while read column sub; do \
+    sub1=`sed -e 's/^"//' -e 's/",$//' <<< $sub` \
+    && echo -e "$user1 $sub1\n" >> /home/dijana/user-mapping.csv\
+    ;done
+
+user2=test-user2
+curl -s -H "Authorization: Bearer $(oidc-token test-user2)" http://keycloak:8080/realms/test-realm/protocol/openid-connect/userinfo | jq . | grep sub | while read column sub; do \
+    sub1=`sed -e 's/^"//' -e 's/",$//' <<< $sub` \
+    && echo -e "$user2 $sub2\n" >> /home/dijana/user-mapping.csv\
+    ;done
