746437bf3aaec018a4307ee10ca8086933100282
vrbanecd <dijana.vrbanec@desy.de>
2023-11-07 14:59:18 +0100

    Debug

--- robot/User-sub_generator.sh
+++ robot/User-sub_generator.sh
@@ -1,11 +1,11 @@
 user1=test-user1
 curl -s -H "Authorization: Bearer $(oidc-token test-user1)" http://keycloak:8080/realms/test-realm/protocol/openid-connect/userinfo | jq . | grep sub | while read column sub; do \
     sub1=`sed -e 's/^"//' -e 's/",$//' <<< $sub` \
-    && echo -e "$user1 $sub1\n" >> /etc/teapot/user-mapping.csv\
+    && echo -e "$user1 $sub1" >> /etc/teapot/user-mapping.csv\
     ;done
 
 user2=test-user2
 curl -s -H "Authorization: Bearer $(oidc-token test-user2)" http://keycloak:8080/realms/test-realm/protocol/openid-connect/userinfo | jq . | grep sub | while read column sub; do \
     sub2=`sed -e 's/^"//' -e 's/",$//' <<< $sub` \
-    && echo -e "$user2 $sub2\n" >> /etc/teapot/user-mapping.csv\
+    && echo -e "$user2 $sub2" >> /etc/teapot/user-mapping.csv\
     ;done
