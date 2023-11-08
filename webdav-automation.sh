3eabd974a54b65b9ec08db51134cefff15aa3929
vrbanecd <dijana.vrbanec@desy.de>
2023-11-08 15:08:13 +0100

    test

--- webdav-automation.sh
+++ webdav-automation.sh
@@ -19,7 +19,7 @@ export USER="$1"
 export port="$2"
 
 echo "StoRM-WebDAV instance for user $USER will be listening on the port $port"
-echo "${{ env.IP_ADDRESS }}"
+echo "$IP_ADDRESS"
 
 #creation of user-specific configuration directories
 USER_DIR=/var/lib/teapot/user-$USER
