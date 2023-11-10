fe016b0b2bbb75ecc26ee651725b0a94bf071b60
vrbanecd <dijana.vrbanec@desy.de>
2023-11-10 13:39:31 +0100

    test

--- webdav-server-start.sh
+++ webdav-server-start.sh
@@ -10,30 +10,30 @@ USER_DIR=/var/lib/teapot/user-"$USER"
 STORM_DIR=/var/lib/teapot/webdav
 
 HOST_IP="$(curl ifconfig.me)"
-echo HOST_IP is "D$HOST_IP"
+echo HOST_IP is "$HOST_IP"
 
 echo "Starting StoRM WebDAV as user $USER"
 export STORM_WEBDAV_JVM_OPTS="-Xms2048M -Xmx2048M -Djava.security.egd=file:/dev/./urandom"
 export STORM_WEBDAV_SERVER_ADDRESS="$HOST_IP"
 export STORM_WEBDAV_HTTPS_PORT="$port"
 export STORM_WEBDAV_HTTP_PORT="1$port" 
-export STORM_WEBDAV_CERTIFICATE_PATH=$STORM_DIR/localhost.crt
-export STORM_WEBDAV_PRIVATE_KEY_PATH=$STORM_DIR/localhost.key
+export STORM_WEBDAV_CERTIFICATE_PATH="$STORM_DIR"/localhost.crt
+export STORM_WEBDAV_PRIVATE_KEY_PATH="$STORM_DIR"/localhost.key
 export STORM_WEBDAV_TRUST_ANCHORS_DIR=/etc/ssl/certs
 export STORM_WEBDAV_TRUST_ANCHORS_REFRESH_INTERVAL=86400
 export STORM_WEBDAV_MAX_CONNECTIONS=300
 export STORM_WEBDAV_MAX_QUEUE_SIZE=900
 export STORM_WEBDAV_CONNECTOR_MAX_IDLE_TIME=30000
-export STORM_WEBDAV_SA_CONFIG_DIR=$USER_DIR/sa.d
+export STORM_WEBDAV_SA_CONFIG_DIR="$USER_DIR"/sa.d
 export STORM_WEBDAV_JAR=/usr/share/java/storm-webdav/storm-webdav-server.jar
 
-export STORM_WEBDAV_LOG=$USER_DIR/log/server.log
-export STORM_WEBDAV_OUT=$USER_DIR/log/server.out
-export STORM_WEBDAV_ERR=$USER_DIR/log/server.err
+export STORM_WEBDAV_LOG="$USER_DIR"/log/server.log
+export STORM_WEBDAV_OUT="$USER_DIR"/log/server.out
+export STORM_WEBDAV_ERR="$USER_DIR"/log/server.err
 
 ETC_DIR=/etc/teapot
-export STORM_WEBDAV_LOG_CONFIGURATION=$ETC_DIR/logback.xml
-export STORM_WEBDAV_ACCESS_LOG_CONFIGURATION=$ETC_DIR/logback-access.xml
+export STORM_WEBDAV_LOG_CONFIGURATION="$ETC_DIR"/logback.xml
+export STORM_WEBDAV_ACCESS_LOG_CONFIGURATION="$ETC_DIR"/logback-access.xml
 export STORM_WEBDAV_VO_MAP_FILES_ENABLE=false
 export STORM_WEBDAV_VO_MAP_FILES_REFRESH_INTERVAL=21600
 export STORM_WEBDAV_TPC_MAX_CONNECTIONS=50
