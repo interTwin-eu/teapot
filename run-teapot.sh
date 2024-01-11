0a80a127b48a8e65369b0a013732f3d7b47928b2
vrbanecd <dijana.vrbanec@desy.de>
2024-01-11 14:29:13 +0100

    Adjustments

--- run-teapot.sh
+++ run-teapot.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-path_to_pid="$HOME/teapot.pid"
 if [ -a $path_to_pid ]; then
         stormpid=`cat $path_to_pid`
         kill $stormpid 
         rm $path_to_pid
 else
-python3 $HOME/teapot/teapot.py &> $HOME/logs/tmp-storm-proxy.log &
+python3 $HOME/teapot/teapot.py &> $/usr/share/teapot/tmp-storm-proxy.log &
         echo $! > $path_to_pid
 fi
