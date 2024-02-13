8ea3f671ae723a47cea08e2a9cef5377e1c6ef49
vrbanecd <dijana.vrbanec@desy.de>
2024-02-13 15:40:06 +0100

    Test 11

--- keycloak-setup/keycloak-config.py
+++ keycloak-setup/keycloak-config.py
@@ -13,23 +13,3 @@ keycloak_admin = KeycloakAdmin(connection=keycloak_connection)
 
 # Changing Realm
 keycloak_connection.realm_name = "test-realm"
-
-# Create a client
-client = keycloak_admin.create_client(
-    payload={
-        "name": "test-client",
-        "clientId": "test-client",
-        "secret": "test-secret",
-        "redirectUris": ["http://localhost:4242/"],
-        "enabled": True,
-        "directAccessGrantsEnabled": True,
-    }
-)
-
-# Configuring client in the new Realm
-keycloak_openid = KeycloakOpenID(
-    server_url="http://keycloak:8080/",
-    client_id="test-client",
-    realm_name="test-realm",
-    client_secret_key="test-secret",
-)
