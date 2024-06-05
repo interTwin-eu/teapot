6ecf8e70e7f0c25e63ee1371da48d057599949a2
vrbanecd <dijana.vrbanec@desy.de>
2024-06-05 17:41:31 +0200

    Tested alternative python java command that satisfies linters 4

--- teapot.py
+++ teapot.py
@@ -323,22 +323,6 @@ async def _start_webdav_instance(username, port):
     # such that it can be managed on its own.
 
     logger.info(f"trying to start process for user {username}.")
-    # sudo_options = f"--preserve-env={','.join(env_pass)} -u {username}"
-    # java = "/usr/bin/java"
-    # jvm_opts = "-Xms2048m -Xmx2048m -Djava.security.egd=file:/dev/./urandom"
-    # storm_webdav_jar = ("/usr/share/java/storm-webdav/storm-webdav-server.jar")
-    # storm_webdav_log_configuration = f"/etc/{APP_NAME}/logback.xml"
-    # spring_loc = f"/var/lib/{APP_NAME}/user-{username}/config/application.yml"
-    # storm_webdav_out = f"/var/lib/{APP_NAME}/user-{username}/log/server.out"
-    # storm_webdav_err = f"/var/lib/{APP_NAME}/user-{username}/log/server.err"
-    # user_t_path = f"/var/lib/user-{username}/t"+"mp"
-
-    # full_cmd = f"sudo {sudo_options} \
-    #     {java} -jar {storm_webdav_jar} {jvm_opts} \
-    #     -Djava.io.tmpdir={user_t_path} \
-    #     -Dlogging.config={storm_webdav_log_configuration} \
-    #     --spring.config.additional-location=optional:file:{spring_loc} \
-    #     1>{storm_webdav_out} 2>{storm_webdav_err} &"
 
     full_cmd = ["sudo", f"--preserve-env={env_pass_quoted}", "-u",
                 f"{username}", "/usr/bin/java", "-jar",
@@ -348,14 +332,19 @@ async def _start_webdav_instance(username, port):
                 f"-Djava.io.tmpdir=/var/lib/user-{username}/tmp",
                 "-Dlogging.config=/etc/teapot/logback.xml",
                 f"--spring.config.additional-location=optional:file:\
-                    /var/lib/teapot/user-{username}/config/application.yml",
-                f"1>f/var/lib/teapot/user-{username}/log/server.out",
-                f"2>/var/lib/teapot/user-{username}/log/server.err",
-                "&"
+                    /var/lib/teapot/user-{username}/config/application.yml"
                 ]
 
+    stdout_file = open(f"/var/lib/teapot/user-{username}/log/server.out", "w")
+    stderr_file = open(f"/var/lib/teapot/user-{username}/log/server.err", "w")
+
     logger.info(f"full_cmd={full_cmd}")
-    p = subprocess.Popen(full_cmd, preexec_fn=os.setsid)
+
+    p = subprocess.Popen(full_cmd, preexec_fn=os.setsid, stdout=stdout_file,
+                         stderr=stderr_file)
+
+    stdout_file.close()
+    stderr_file.close()
 
     # wait for it...
     await anyio.sleep(1)
