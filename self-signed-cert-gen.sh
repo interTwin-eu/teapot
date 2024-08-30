b525476c44015d4e198d6b3e498e06946125c902
vrbanecd <dijana.vrbanec@desy.de>
2024-08-30 11:50:49 +0200

    test 2

--- self-signed-cert-gen.sh
+++ self-signed-cert-gen.sh
@@ -5,10 +5,11 @@ mkdir -p "$path"
 
 openssl req -newkey rsa:2048 -keyout "$path"/lolcahost.key -x509 -days 365 -out "$path"/localhost.crt -subj "/C=. /ST=. /L=. /O=. Corp/OU=. Dept/CN=localhost"
 
-chmod ugo+r "$path"/localhost.key
+chmod 644 "$path"/localhost.key
 
 path1=/etc/pki/ca-trust/source/anchors
 cp "$path"/localhost.crt "$path1"/localhost.crt
+chmod 744 "$path1"/localhost.crt
 update-ca-trust
 
 crontab -l | { cat; echo "0 0 1 1 * sudo cert_generation_localhost.sh"; } | crontab -
