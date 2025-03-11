044ef1bf56ca1a3d15e6f7c452b3a62950145334
vrbanecd <dijana.vrbanec@desy.de>
2025-03-11 16:00:20 +0100

    Changed storm-webdav version to 1.6.0

--- rpm/teapot-rpm-alma.sh
+++ rpm/teapot-rpm-alma.sh
@@ -4,30 +4,29 @@
 rpmdev-setuptree
 
 #adding a spec file
-cp rpm/teapot-alma.spec "$HOME"/rpmbuild/SPECS/
+cp rpm/teapot-alma.spec "${HOME}"/rpmbuild/SPECS/
 
 #getting the teapot scripts
-mkdir teapot-"$version_rpm"
-rsync -av --progress * teapot-"$version_rpm"/ --exclude teapot-"$version_rpm"
-tar cfz teapot-"$version_rpm".tar.gz teapot-"$version_rpm"
-rm -r teapot-"$version_rpm"
-mv teapot-"$version_rpm".tar.gz "$HOME"/rpmbuild/SOURCES/
+mkdir teapot-"${version_rpm}"
+rsync -av --progress * teapot-"${version_rpm}"/ --exclude teapot-"${version_rpm}"
+tar cfz teapot-"${version_rpm}".tar.gz teapot-"${version_rpm}"
+rm -r teapot-"${version_rpm}"
+mv teapot-"${version_rpm}".tar.gz "${HOME}"/rpmbuild/SOURCES/
 
 #getting the storm-webdav.jar file
-curl -O https://repo.cloud.cnaf.infn.it/repository/storm-rpm-stable/redhat8/storm-webdav-1.5.0-1.el8.noarch.rpm
-#curl -O https://syncandshare.desy.de/index.php/s/GwSKbqF8DQZ4KzG/download/storm-webdav-1.4.2-1.el7.noarch.rpm
-rpm2cpio storm-webdav-1.5.0-1.el8.noarch.rpm | cpio -id ./usr/share/java/storm-webdav/storm-webdav-server.jar
-rm storm-webdav-1.5.0-1.el8.noarch.rpm
+curl -O https://repo.cloud.cnaf.infn.it/repository/storm-rpm-stable/redhat8/storm-webdav-1.6.0-1.el8.noarch.rpm
+rpm2cpio storm-webdav-1.6.0-1.el8.noarch.rpm | cpio -id ./usr/share/java/storm-webdav/storm-webdav-server.jar
+rm storm-webdav-1.6.0-1.el8.noarch.rpm
 mv usr/share/java/storm-webdav/storm-webdav-server.jar ./
 rm -r usr
 tar cf storm-webdav-server.tar.gz storm-webdav-server.jar
 rm storm-webdav-server.jar
-mv storm-webdav-server.tar.gz "$HOME"/rpmbuild/SOURCES/
+mv storm-webdav-server.tar.gz "${HOME}"/rpmbuild/SOURCES/
 
-cd /tmp
+cd /tmp || exit
 curl -O https://syncandshare.desy.de/index.php/s/ZE6M6ZDYPoftnbG/download/python-lib64-alma1.tar.gz .
 curl -O https://syncandshare.desy.de/index.php/s/WdEpm9idqqYy8Sp/download/python-lib-alma1.tar.gz .
-mv /tmp/python-lib*.tar.gz "$HOME"/rpmbuild/SOURCES/
+mv /tmp/python-lib*.tar.gz "${HOME}"/rpmbuild/SOURCES/
 
 #building the RPM package
-rpmbuild -ba --define "version_ $version_rpm" ~/rpmbuild/SPECS/teapot-alma.spec
+rpmbuild -ba --define "version_ ${version_rpm}" ~/rpmbuild/SPECS/teapot-alma.spec
