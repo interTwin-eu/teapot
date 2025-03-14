b1216adfd32a3a96e59aeac4f4eb5ca72151a314
vrbanecd <dijana.vrbanec@desy.de>
2025-03-14 15:40:04 +0100

    Corrected a minor error in RuntimeError

--- alise.py
+++
@@ -1,7 +0,0 @@
-class Alise(sub):
-    def __init__(self, sub):
-        self.apikey = config["Teapot"]["ALISE_APIKEY"]
-        self.issuer = config["Teapot"]["ALISE_ISSUER"]
-        self.sub = sub
-    def get_local_username:
-        
