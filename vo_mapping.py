4e258afbe52ef599e100ce4e203f15b40e733644
Dijana Vrbanec <127301388+vrbanecd@users.noreply.github.com>
2025-04-17 17:44:25 +0200

    Adding vo membership based mapping (#41)
    * Added an option to chose mapping
    
    * Added alise integration
    
    * Added alise.py and requests library to spec files. Corrected linting errors.
    
    * Added requests library to the teapot-requirements image, corrected linting
    
    * Changed some methods in Alise to staticmethods
    
    * Added ALISE section in configi.ini file, refined ALISE api link creation
    
    * Linted alise.py; changed to querry if user_mapping exist only for FILE mapping; added sub as variable in _create_user_dirs function
    
    * Added sub as a parameter in all the function needed to suport previous change
    
    * Added information about ALISE to the  _map_fed_to_local function docstring. Added logging for ALISE mapping.
    
    * Added logger to alise
    
    * Added logging and error messages to ALISE. Added additional error message for FILE mapping.
    
    * test-debug
    
    * Corrected the wrong indent in user mapping for FILE method
    
    * Changed where ConnectionError is triggered
    
    * Updated exceptions to request to ALISE API
    
    * Removed some unnecessary comments
    
    * Added issuer retrival from user-info to be used by ALISE
    
    * Added ALISE configuration and additional information on starting Teapot.
    
    * Corrected a line-length error.
    
    * Added path to teapot's session state to config.ini
    
    * Added mapping by VO groups
    
    * Added some linting, change the new functions to be static, and changed the default mapping to be "FILE".
    
    * Changed the filename to vo_mapping.py
    
    * Added vo_mapping to the rpms. Also did some linting.
    
    * Updated VO mapping
    
    * Added a default value for 'VO_member' variable
    
    * Changed where I defined VO_member variable
    
    * Updated group recognition in get_local_username
    
    * Updated VO mapping
    
    * Corrected a variable name
    
    * Corrected identation error and linted
    
    * Corrected a variable call in logging
    
    * Updated querry for eduperson_entitlement
    
    * Corrected indentation in line 917
    
    * Updated VO_mapping's get_local_username function
    
    * Linted too long line and wrong line indentation
    
    * Updated local_username assignment in vo_mapping.py; Updated storm_webdav_state function to not attempt to start storm-webdav when user is None
    
    * Linted vo_mapping.py
    
    * Updated VO mapping
    
    * Linted VO mapping
    
    * Updated logger message for VO-mapping; added VO-mapping to documentation
    
    * Updated tag

---
+++ vo_mapping.py
@@ -0,0 +1,66 @@
+import configparser
+import logging
+
+config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
+config.read("/etc/teapot/config.ini")
+
+logging.basicConfig(
+    filename=config["Teapot"]["log_location"],
+    encoding="utf-8",
+    filemode="a",
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M",
+    level=config["Teapot"]["log_level"],
+)
+logger = logging.getLogger(__name__)
+
+
+class VOMapping:
+    def __init__(self, eduperson_entitlement):
+        self.eduperson_entitlement = [
+            ent.split("#")[0] for ent in eduperson_entitlement if ent
+        ]
+        logger.debug(
+            "User's eduperson entitlements after the hash has been removed are %s",
+            self.eduperson_entitlement,
+        )
+
+    def get_local_username(self, sub: str):
+        logger.info(
+            "Checking if the user with sub %s is a member of a specified VO", sub
+        )
+        for entitlement in self.eduperson_entitlement:
+            for group in config["VO_enforcement"]:
+                if group.startswith("group_"):
+                    group_requirement = config["VO_enforcement"][group].split("#")[0]
+                    if entitlement == group_requirement:
+                        logger.info(
+                            "User with sub %s is a member of the specified VO %s",
+                            sub,
+                            group_requirement,
+                        )
+                        parts = group.split("_", 1)
+                        if len(parts) != 2:
+                            logger.warning("Group %s is not named correctly", group)
+                            continue
+                        group_tag = parts[1]
+                        try:
+                            local_username = config["VO_enforcement"][
+                                "username_" + group_tag
+                            ]
+                            logger.info(
+                                "The local group account mapped to VO group %s is %s ",
+                                entitlement,
+                                local_username,
+                            )
+                            return local_username
+                        except KeyError:
+                            logger.error(
+                                "No local username mapping found for VO group %s."
+                                + "Username_%s not defined in config.",
+                                entitlement,
+                                group_tag,
+                            )
+
+        logger.error("User does not meet VO membership requirements")
+        return None
