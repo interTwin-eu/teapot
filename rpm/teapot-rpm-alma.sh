00635d662db443b2da82b4b37d3ad2bb8487e776
Dijana Vrbanec <127301388+vrbanecd@users.noreply.github.com>
2025-04-29 17:22:25 +0200

    New configuration creation (#42)
    * Updated storage_area.properties file creation
    
    * Updated automation process for creating application.yml
    
    * Moved user_config.ini creation to _create_user_dirs; linted, deleted no longer needed templates
    
    * Removed the no-longer-needed files from the rpm packaging
    
    * Updated configuration description based on the new templates

--- rpm/teapot-rpm-alma.sh
+++ rpm/teapot-rpm-alma.sh
@@ -4,14 +4,14 @@
 rpmdev-setuptree
 
 #adding a spec file
-cp rpm/teapot-alma.spec "$HOME"/rpmbuild/SPECS/
+cp rpm/teapot-alma.spec "${HOME}"/rpmbuild/SPECS/
 
 #getting the teapot scripts
-mkdir teapot-"$version_rpm"
-rsync -av --progress * teapot-"$version_rpm"/ --exclude teapot-"$version_rpm"
-tar cfz teapot-"$version_rpm".tar.gz teapot-"$version_rpm"
-rm -r teapot-"$version_rpm"
-mv teapot-"$version_rpm".tar.gz "$HOME"/rpmbuild/SOURCES/
+mkdir teapot-"${version_rpm}"
+rsync -av --progress * teapot-"${version_rpm}"/ --exclude teapot-"${version_rpm}"
+tar cfz teapot-"${version_rpm}".tar.gz teapot-"${version_rpm}"
+rm -r teapot-"${version_rpm}"
+mv teapot-"${version_rpm}".tar.gz "${HOME}"/rpmbuild/SOURCES/
 
 #getting the storm-webdav.jar file
 curl -O https://repo.cloud.cnaf.infn.it/repository/storm-rpm-stable/redhat8/storm-webdav-1.5.0-1.el8.noarch.rpm
@@ -22,12 +22,12 @@ mv usr/share/java/storm-webdav/storm-webdav-server.jar ./
 rm -r usr
 tar cf storm-webdav-server.tar.gz storm-webdav-server.jar
 rm storm-webdav-server.jar
-mv storm-webdav-server.tar.gz "$HOME"/rpmbuild/SOURCES/
+mv storm-webdav-server.tar.gz "${HOME}"/rpmbuild/SOURCES/
 
-cd /tmp
+cd /tmp || exit
 curl -O https://syncandshare.desy.de/index.php/s/ZE6M6ZDYPoftnbG/download/python-lib64-alma1.tar.gz .
 curl -O https://syncandshare.desy.de/index.php/s/WdEpm9idqqYy8Sp/download/python-lib-alma1.tar.gz .
-mv /tmp/python-lib*.tar.gz "$HOME"/rpmbuild/SOURCES/
+mv /tmp/python-lib*.tar.gz "${HOME}"/rpmbuild/SOURCES/
 
 #building the RPM package
-rpmbuild -ba --define "version_ $version_rpm" ~/rpmbuild/SPECS/teapot-alma.spec
+rpmbuild -ba --define "version_ ${version_rpm}" ~/rpmbuild/SPECS/teapot-alma.spec
