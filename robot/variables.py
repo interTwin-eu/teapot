cf91fb2ac22116f542cceff44ce94046c7a45481
vrbanecd <dijana.vrbanec@desy.de>
2025-05-12 16:04:46 +0200

    Change the token header to conform to Tomcat 10 in Spring Boot 3

--- robot/variables.py
+++ robot/variables.py
@@ -5,7 +5,7 @@ DEFAULT_AREA = "https://teapot:8081/default_area"
 EXTRA_AREA = "https://teapot:8081/extra_area"
 TOKEN1 = subprocess.check_output("oidc-token test-user1", shell=True)
 TOKEN2 = subprocess.check_output("oidc-token test-user2", shell=True)
-HEADER1 = {"Authorization": ("Bearer " + str(TOKEN1, "ascii")).strip("\n")}
-HEADER2 = {"Authorization": ("Bearer " + str(TOKEN2, "ascii")).strip("\n")}
+HEADER1 = {"Authorization": "Bearer " + str(TOKEN1, "ascii").strip()}
+HEADER2 = {"Authorization": "Bearer " + str(TOKEN2, "ascii").strip()}
 HEADER3 = {"Authorization": "Bearer NOT_A_TOKEN"}
 DATA = "Hello! This is a test. "
