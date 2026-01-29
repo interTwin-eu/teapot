a451a90fc7ac2821f6ed4b51074d1a143d35bd9f
vrbanecd <dijana.vrbanec@desy.de>
2026-01-29 14:35:32 +0100

    Updated crt creation for teapot-ubuntu to include SAN

--- robot/ubuntu/cert-gen.sh
+++ robot/ubuntu/cert-gen.sh
@@ -4,14 +4,23 @@ set -euo pipefail
 SERVER="$1"
 
 if [[ -z "${SERVER}" ]]; then
-  echo "Usage: $0 teapot-ubuntu"
+  echo "Usage: $0 <server-name>"
   exit 1
+fi
+
+CA_CERT="Teapot-testing.crt"
+CA_KEY="Teapot-testing.key"
+
+
+if [[ ! -f "${CA_CERT}" || ! -f "${CA_KEY}" ]]; then
+    echo "CA certificate or key not found in current directory"
+    exit 1
 fi
 
 # server key
 openssl genrsa -out "teapot.key" 2048
 
-# CSR {no prompts}
+# CSR
 openssl req -new \
   -key teapot.key \
   -subj "/CN=${SERVER}" \
@@ -19,6 +28,14 @@ openssl req -new \
   -addext "keyUsage=digitalSignature,keyEncipherment" \
   -addext "extendedKeyUsage=serverAuth" \
   -out "teapot.csr"
+
+# ext file to include SAN when signing
+cat > teapot.ext.cnf <<EOF
+[ v3_req ]
+subjectAltName = DNS:${SERVER}
+keyUsage = digitalSignature,keyEncipherment
+extendedKeyUsage = serverAuth
+EOF
 
 # sign with CA (no prompts)
 openssl x509 -req \
@@ -28,4 +45,6 @@ openssl x509 -req \
   -CAcreateserial \
   -out "teapot.crt" \
   -days 1 \
-  -sha256
+  -sha256 \
+  -extfile teapot.ext.cnf \
+  -extensions v3_req
