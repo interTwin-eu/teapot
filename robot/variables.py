b9d984d4508ce8f2eba9089910d5abbd0ad90b89
vrbanecd <dijana.vrbanec@desy.de>
2026-03-17 15:24:20 +0100

    Updated debian testing

--- robot/variables.py
+++ robot/variables.py
@@ -3,9 +3,6 @@ import subprocess
 TEAPOT_URL = "https://teapot:8081"
 DEFAULT_AREA = "https://teapot:8081/default"
 DATA_AREA = "https://teapot:8081/data"
-TEAPOT_DEB_URL = "https://teapot-ubuntu:8081"
-DEFAULT_DEB_AREA = "https://teapot-ubuntu:8081/default"
-DATA_DEB_AREA = "https://teapot-ubuntu:8081/data"
 TOKEN1 = subprocess.check_output("oidc-token test-user1", shell=True)
 TOKEN2 = subprocess.check_output("oidc-token test-user2", shell=True)
 TOKEN3 = subprocess.check_output("oidc-token test-user3", shell=True)
