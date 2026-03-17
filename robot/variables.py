8b4685466e4893eb54ef8b0ee564e4ee081b65d4
vrbanecd <dijana.vrbanec@desy.de>
2026-03-17 11:36:12 +0100

    Re-added all the user configurations to all testing jobs

--- robot/variables.py
+++ robot/variables.py
@@ -10,9 +10,13 @@ TOKEN1 = subprocess.check_output("oidc-token test-user1", shell=True)
 TOKEN2 = subprocess.check_output("oidc-token test-user2", shell=True)
 TOKEN3 = subprocess.check_output("oidc-token test-user3", shell=True)
 TOKEN4 = subprocess.check_output("oidc-token test-user4", shell=True)
+TOKEN_A1 = subprocess.check_output("oidc-token alise-user1", shell=True)
+TOKEN_A2 = subprocess.check_output("oidc-token alise-user2", shell=True)
 HEADER0 = {"Authorization": "Bearer NOT_A_TOKEN"}
 HEADER1 = {"Authorization": "Bearer " + str(TOKEN1, "ascii").strip()}
 HEADER2 = {"Authorization": "Bearer " + str(TOKEN2, "ascii").strip()}
 HEADER3 = {"Authorization": "Bearer " + str(TOKEN3, "ascii").strip()}
 HEADER4 = {"Authorization": "Bearer " + str(TOKEN4, "ascii").strip()}
+HEADER_A1 = {"Authorization": "Bearer " + str(TOKEN_A1, "ascii").strip()}
+HEADER_A2 = {"Authorization": "Bearer " + str(TOKEN_A2, "ascii").strip()}
 DATA = "A" * 1024
